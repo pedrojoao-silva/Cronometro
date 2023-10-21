71991d5e85c142f135854716a6a165851a945925
pedrojoao-silva <joaopedro.pmwto@gmail.com>
2023-10-21 11:41:36 -0300

    Finalizando o Cronometro.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+            
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int mult = 1;
+
+            if(type == 'm')
+                mult = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * mult);
+            
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronometro Finalizado.");
+            Thread.Sleep(1000);
+            Menu();
+        }
+    }
+}
